08e4c65f844159244db3fcfba47c97e88518afd6
Ankita Saha <sahaankita348@gmail.com>
2022-04-01 16:52:51 -0400

    raise added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of the salary you want to save, in decimal: "))
 total_cost = float(input("Enter the total cost of the house: "))
+quarter_raise = float(input("Enter your quarterly raise, in decimal:"))
 
 
 portion_down_payment = 0.25
@@ -16,4 +17,5 @@ monthly_savings = (annual_salary/12) * portion_saved
 while current_savings < down_payment:
     current_savings += ((current_savings * r) / 12) + monthly_savings
     month += 1
+
 print("The number of months it will take is: " + str(month))
